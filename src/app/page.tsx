e9ade91211615d82301049d92260c0c52556ef2b
SurafelZemedkun <164858071+SurafelZemedkun@users.noreply.github.com>
2026-07-17 08:29:19 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from '../../components/Header';
-import Footer from '../..components/Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import HeroSection from './components/HeroSection';
 import ServicesSection from './components/ServicesSection';
 import SuccessStoriesSection from './components/SuccessStoriesSection';
-import WhyUsSection from './app/components/WhyUsSection';
+import WhyUsSection from './components/WhyUsSection';
 import BookingSection from './components/BookingSection';
 
 export default function HomePage() {
