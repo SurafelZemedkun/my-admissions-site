f96cb2c45ebff2e215bcb86395fed6461c4b93f7
SurafelZemedkun <164858071+SurafelZemedkun@users.noreply.github.com>
2026-07-17 07:55:21 +0300

    Update BookingSection.tsx

--- src/app/components/BookingSection.tsx
+++ src/app/components/BookingSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect, useRef } from 'react';
-import Icon from '@/components/ui/AppIcon';
+import Icon from './../components/ui/AppIcon';
 
 const steps = [
   {
