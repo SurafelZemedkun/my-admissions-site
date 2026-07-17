3d4f8cb6a4247f5242cd34d69ebf95cb79a76764
SurafelZemedkun <164858071+SurafelZemedkun@users.noreply.github.com>
2026-07-17 08:35:59 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
-import AppLogo from '@/components/ui/AppLogo';
-import Icon from '@/components/ui/AppIcon';
+import AppLogo from './ui/AppLogo';
+import Icon from './ui/AppIcon';
 
 export default function Footer() {
   return (
